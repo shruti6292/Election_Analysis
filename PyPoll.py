0b8578ae940ab51469be92a4c69cfb04ae706737
shruti6292 <shrutinp62@gmail.com>
2022-02-23 11:50:25 -0500

    All files added

---
+++ PyPoll.py
@@ -0,0 +1,14 @@
+
+# Create a filename variable to a direct or indirect path to the file.
+import os
+
+
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Use the open statement to open the file as a text file.
+outfile = open(file_to_save, "w")
+# Write some data to the file.
+outfile.write("Hello World")
+
+# Close the file
+outfile.close()
